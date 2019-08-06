4de7ab662aa54dd7464664724be1d5f3a4336da8
Hyo Gyeong <cdkrcd8@gmail.com>
2019-08-06 17:56:47 +0900

    model에 import추가-이미지 안보임 수정

--- blogpj/settings.py
+++ blogpj/settings.py
@@ -125,7 +125,7 @@ TIME_ZONE = 'Asia/Seoul'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'portfolio', 'static')
+    os.path.join(BASE_DIR, 'blog', 'static')
 ] #static파일들이 현재 어디에 있는지 쓰는 곳
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
